b79d2cca91738cca9f2b6744e65f041b5806b62a
ghoulx <150254673+ghoulxx@users.noreply.github.com>
2025-02-03 12:30:18 -0600

    Update Glock.js

--- cheats/Glock.js
+++ cheats/Glock.js
@@ -6,8 +6,7 @@
  * it under the terms of the GNU Affero General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
+ * https://github.com/ghoulxx/trapping-cheats/blob/main/cheats/Glock.js * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
